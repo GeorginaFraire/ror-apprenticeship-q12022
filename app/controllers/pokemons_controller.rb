7a966e8484432ef6d832cd598345d8fc55f332d0
Georgina Fraire Miranda <georgina.fraire@wizeline.com>
2022-01-17 18:40:18 -0600

    Add image to Pokemon/Index

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,7 +3,7 @@ class PokemonsController < ApplicationController
 
   # GET /pokemons or /pokemons.json
   def index
-    @pokemons = Pokemon.all
+    @pokemons = Pokemon.all.name_sort
   end
 
   # GET /pokemons/1 or /pokemons/1.json
