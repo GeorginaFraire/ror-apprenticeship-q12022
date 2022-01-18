9e4c9b4262f94ed924e29a7948c90a074a1837af
Georgina Fraire Miranda <georgina.fraire@wizeline.com>
2022-01-17 18:02:04 -0600

    Delete files AbilityPokemon  and PokemonType Add files ability_pokemon and pokemon_type Add Data Validation Add Named Scopes

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,17 @@
 class Pokemon < ApplicationRecord
+
+  validates :name, :presence => true,  :length => {:maximum => 255}
+  validates :order, :presence => true, :numericality => true
+  validates :base_expirence, :presence => true, :numericality => true
+  validates :heigth, :presence => true, :numericality => true
+  validates :weight, :presence => true, :numericality => true
+
+  scope :name_sort, -> {order(:name)}
+
+  has_many :ability_pokemons
+  has_many :abilities, :through => :ability_pokemons
+
   has_many :pokemon_types
   has_many :types, :through => :pokemon_types
+  
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,9 @@
 class Type < ApplicationRecord
+  
+  validates :name, :presence => true, :length => {:maximum => 255}
+
+  scope :name_sort, -> {order(:name)}
+
   has_many :pokemon_types
   has_many :pokemons, :through => :pokemon_types
 end
