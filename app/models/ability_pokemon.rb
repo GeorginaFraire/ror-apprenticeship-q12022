bfb566beccbe4768b8c3814bb28be0f647bee769
Georgina Fraire Miranda <georgina.fraire@wizeline.com>
2022-01-21 14:31:37 -0600

    Add style in Pokemon index

--- app/models/ability_pokemon.rb
+++ app/models/ability_pokemon.rb
@@ -1,4 +1,5 @@
 class AbilityPokemon < ApplicationRecord
+    validates :ability_id, :presence => true, :numericality => true, :uniqueness => { scope: :pokemon_id }
     
     belongs_to :ability
     belongs_to :pokemon
