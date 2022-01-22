858fce3b344e7e80e1f941fb8325b6de5ef0bdc2
Georgina Fraire Miranda <georgina.fraire@wizeline.com>
2022-01-21 23:23:52 -0600

    Add Authentication - Workshop implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
+  before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:login_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:login_name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root 'pokemons#index'
   resources :types
   resources :abilities
   resources :pokemons
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
   
   resources :pokemons do 
     member do 
-      put :addAbility
-      put :addType
-      delete :deleteAbility
-      delete :deleteType
+      put :add_ability
+      put :add_type
+      delete :delete_ability
+      delete :delete_type
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
